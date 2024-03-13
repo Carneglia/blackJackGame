96551269c647e0f0be7cbb73e76b7c20a5140f81
Carlos Carneglia <cd.carneglia@gmail.com>
2024-03-13 15:12:29 -0300

    starting the proyect

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const numeroTiros = 3;
+const escalera1 = 23456; 
+const escalera2 = 12345; 
+const escalera3 = 13456;
+let full; 
+
+let num1 = Math.floor(Math.random()*6) +1
+let num2 = Math.floor(Math.random()*6) +1
+let num3 = Math.floor(Math.random()*6) +1
+let num4 = Math.floor(Math.random()*6) +1
+let num5 = Math.floor(Math.random()*6) +1
+let num6 = Math.floor(Math.random()*6) +1
+
+function () => {
+    if( num1 === )
+}
+
+for(let i = 0; i <= 6; i++){
+    if 
+}
+
+
+while(true){
+
+}
