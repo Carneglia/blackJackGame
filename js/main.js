3a19c16c9d3db1197b54c51c0bfd7ea9b3fdc901
Carlos Carneglia <cd.carneglia@gmail.com>
2024-03-13 20:23:05 -0300

    main code was added

--- js/main.js
+++ js/main.js
@@ -1,25 +1,68 @@
-const numeroTiros = 3;
-const escalera1 = 23456; 
-const escalera2 = 12345; 
-const escalera3 = 13456;
-let full; 
+const cardsEl = document.getElementById("cards-el");
+const sumEl = document.getElementById("sum-el");
+const startEl = document.getElementById("start-el");
+const messageEl = document.getElementById("message-el");
+let newCardEl = document.getElementById("extra-el");
+let hasBlackJack = false
+let playerAlive = false;
+let playerLost = true;
+const player = "";
+let cards = [];
+let message = ""
+let sum = ""
 
-let num1 = Math.floor(Math.random()*6) +1
-let num2 = Math.floor(Math.random()*6) +1
-let num3 = Math.floor(Math.random()*6) +1
-let num4 = Math.floor(Math.random()*6) +1
-let num5 = Math.floor(Math.random()*6) +1
-let num6 = Math.floor(Math.random()*6) +1
+function getRandomCard() {
+    let randomNumer = Math.floor(Math.random() * 11) + 1;
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+function renderGame() {
+    cardsEl.textContent = ""
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += "- " + cards[i] + " "
+    }
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+        newCardEl.style.display = "block";
+        playerAlive = true;
 
-function () => {
-    if( num1 === )
+    } else if (sum === 21) {
+        message = "YOU WIN !"
+        hasBlackJack = true;
+        startEl.style.display = "block";
+        newCardEl.style.display = "none";
+    } else {
+        message = "Sorry, you lost !"
+        playerAlive = false;
+        startEl.style.display = "block"
+        newCardEl.style.display = "none";
+    }
+    messageEl.textContent = message
 }
 
-for(let i = 0; i <= 6; i++){
-    if 
+function startGame() {
+    playerAlive = true;
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard;
+    sumEl.textContent = sum
+    renderGame()
+    startEl.style.display = "none"
 }
+function extraCard() {
+    if (playerAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        cardsEl.textContent = cards
+        sum += card
+        cards.push(card)
+        sumEl.textContent = sum
+        renderGame()
 
-
-while(true){
-
+    }
 }
